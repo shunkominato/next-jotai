06ce20edafab3e6ea8068cc9c8d1540a7007e766
shunkominato <shun19871001@gmail.com>
2023-02-25 09:16:26 +0900

    d

--- src/page-contents/SignUp/SignUp.tsx
+++ src/page-contents/SignUp/SignUp.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import Link from 'next/link';
 import { Container, Paper } from '@mantine/core';
 import { FormTitle } from '@/components/ui/elements/form/form-title/FormTitle';
 import { SignUpForm } from './signUpForm';
@@ -10,6 +11,11 @@ export const SignUp: FC = () => {
         <div className="px-20 py-9">
           <FormTitle label="新規ユーザー登録" />
           <SignUpForm />
+          <div className="mt-6 text-center">
+            <Link href="/" className="no-underline">
+              アカウントをお持ちの方はこちら
+            </Link>
+          </div>
         </div>
       </Paper>
     </Container>
--- src/page-contents/SignUp/signUpForm/SignUpForm.tsx
+++ src/page-contents/SignUp/signUpForm/SignUpForm.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import Link from 'next/link';
 import { useForm, zodResolver } from '@mantine/form';
 import { Divider, PasswordInput, Text } from '@mantine/core';
 import { Button } from '@/components/ui/elements/button/Button';
@@ -24,48 +23,41 @@ export const SignUpForm: FC = () => {
   const { handleSignUp, isLoading, isError } = useSignUp();
 
   return (
-    <>
-      <form className="mt-6" onSubmit={form.onSubmit(handleSignUp)}>
-        <TextInput
-          id="email"
-          label="メールアドレス（半角英数字）"
-          required
-          placeholder="example@gmail.com"
-          form={form}
+    <form className="mt-6" onSubmit={form.onSubmit(handleSignUp)}>
+      <TextInput
+        id="email"
+        label="メールアドレス（半角英数字）"
+        required
+        placeholder="example@gmail.com"
+        form={form}
+      />
+      <PasswordInput
+        mt="md"
+        id="password"
+        label="パスワード"
+        required
+        radius="xs"
+        {...form.getInputProps('password')}
+      />
+      <Text fz="xs">小文字・大文字・数値を含む半角英数字8文字以上</Text>
+      <Text fz="xs" c="gray.6" mt="md">
+        新規登録することで、利用規約 /
+        プライバシーポリシーに同意したとみなします
+      </Text>
+      <div className="mt-4">
+        <Button
+          type="submit"
+          label="登録"
+          size="md"
+          fullWidth
+          disabled={!!form.errors.email || !form.values.email}
+          loading={isLoading}
         />
-        <PasswordInput
-          mt="md"
-          id="password"
-          label="パスワード"
-          required
-          radius="xs"
-          {...form.getInputProps('password')}
-        />
-        <Text fz="xs">小文字・大文字・数値を含む半角英数字8文字以上</Text>
-        <Text fz="xs" c="gray.6" mt="md">
-          新規登録することで、利用規約 /
-          プライバシーポリシーに同意したとみなします
-        </Text>
-        <div className="mt-4">
-          <Button
-            type="submit"
-            label="登録"
-            size="md"
-            fullWidth
-            disabled={!!form.errors.email || !form.values.email}
-            loading={isLoading}
-          />
-        </div>
-        <div className="p-6">
-          <Divider label="または" labelPosition="center" />
-        </div>
-        <TwitterButton />
-      </form>
-      <div className="mt-6 text-center">
-        <Link href="/" className="no-underline">
-          アカウントをお持ちの方はこちら
-        </Link>
       </div>
-    </>
+      <div className="p-6">
+        <Divider label="または" labelPosition="center" />
+      </div>
+      <TwitterButton />
+    </form>
   );
 };
